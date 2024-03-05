db140fa9b1d5359e8caa3c99273dd213aa850fb4
Icaro Siqueira <icaro.siqueira@rebase.com.br>
2024-03-05 12:42:26 -0300

    Add rubocop and apply its fixes

--- Gemfile
+++ Gemfile
@@ -1,18 +1,23 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.2.3"
+ruby '3.2.3'
 
-gem "rails", "7.1.2"
-gem "sprockets-rails"
-gem "pg", "~> 1.1"
-gem "puma", "~> 6.0"
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 6.0'
+gem 'rails', '7.1.2'
+gem 'sprockets-rails'
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "factory_bot_rails"
-  gem "pry"
-  gem "pry-byebug"
-  gem "rspec-rails"
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'rspec-rails'
+  gem 'rubocop'
+  gem 'rubocop-rails'
+  gem 'rubocop-rspec'
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,110 +1,120 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    subject { get '/' }
+    subject(:request) { get '/' }
 
     it 'returns 200' do
-      subject
+      request
 
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'GET /posts/new' do
-    subject { get '/posts/new' }
+    subject(:request) { get '/posts/new' }
 
     it 'returns 200' do
-      subject
+      request
 
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'POST /posts' do
-    subject { post '/posts', params: { post: { body: body, title: title } } }
+    subject(:request) { post '/posts', params: { post: { body: body, title: title } } }
+
     let(:body) { 'This is the body from some post.' }
     let(:title) { 'Some post title' }
 
     it 'returns 302' do
-      subject
+      request
 
       expect(response).to have_http_status(:found)
     end
 
     it 'creates the Post successfully' do
-      expect{ subject }.to change { Post.count }.from(0).to(1)
+      expect { request }.to change(Post, :count).from(0).to(1)
     end
+
     it 'redirects to post show' do
-      subject
+      request
 
       expect(response).to redirect_to(post_path(id: Post.last.id))
     end
   end
 
   describe 'GET /posts/:id' do
-    subject { get "/posts/#{post.id}" }
+    subject(:request) { get "/posts/#{post.id}" }
+
     let(:post) { create(:post) }
 
     it 'returns 200' do
-      subject
+      request
 
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'GET /posts/:id/edit' do
-    subject { get "/posts/#{post.id}/edit" }
+    subject(:request) { get "/posts/#{post.id}/edit" }
+
     let(:post) { create(:post) }
 
     it 'returns 200' do
-      subject
+      request
 
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'PUT /posts/:id' do
-    subject { put "/posts/#{post.id}", params: { post: { title: new_title } } }
+    subject(:request) do
+      put "/posts/#{post.id}", params: { post: { title: new_title } }
+    end
+
     let(:post) { create(:post, title: old_title) }
     let(:old_title) { 'Post wront title' }
     let(:new_title) { 'Post new title' }
 
     it 'returns 303' do
-      subject
+      request
 
       expect(response).to have_http_status(:see_other)
     end
 
     it 'updates the Post successfully' do
-      subject
+      request
 
       expect(post.reload.title).to eq(new_title)
     end
 
     it 'redirects to post show' do
-      subject
+      request
 
       expect(response).to redirect_to(post_path(id: post.id))
     end
   end
 
   describe 'DELETE /posts/:id' do
-    subject { delete "/posts/#{post.id}" }
+    subject(:request) { delete "/posts/#{post.id}" }
+
     let!(:post) { create(:post) }
 
     it 'returns 303' do
-      subject
+      request
 
       expect(response).to have_http_status(:see_other)
     end
 
     it 'deletes the Post successfully' do
-      expect{ subject }.to change { Post.count }.from(1).to(0)
+      expect { request }.to change(Post, :count).from(1).to(0)
     end
 
     it 'redirects to post show' do
-      subject
+      request
 
       expect(response).to redirect_to(posts_path)
     end
