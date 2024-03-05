444f19455de49684204e5b9ff38b666bfcff5b40
Icaro Siqueira <icaro.siqueira@rebase.com.br>
2024-03-05 12:34:03 -0300

    Add rspec tests, factory bot and remove old tests

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    title { 'Title' }
+    body { 'This is the test body from the Post :)' }
+  end
+end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    subject { get '/' }
+
+    it 'returns 200' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /posts/new' do
+    subject { get '/posts/new' }
+
+    it 'returns 200' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /posts' do
+    subject { post '/posts', params: { post: { body: body, title: title } } }
+    let(:body) { 'This is the body from some post.' }
+    let(:title) { 'Some post title' }
+
+    it 'returns 302' do
+      subject
+
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'creates the Post successfully' do
+      expect{ subject }.to change { Post.count }.from(0).to(1)
+    end
+    it 'redirects to post show' do
+      subject
+
+      expect(response).to redirect_to(post_path(id: Post.last.id))
+    end
+  end
+
+  describe 'GET /posts/:id' do
+    subject { get "/posts/#{post.id}" }
+    let(:post) { create(:post) }
+
+    it 'returns 200' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /posts/:id/edit' do
+    subject { get "/posts/#{post.id}/edit" }
+    let(:post) { create(:post) }
+
+    it 'returns 200' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'PUT /posts/:id' do
+    subject { put "/posts/#{post.id}", params: { post: { title: new_title } } }
+    let(:post) { create(:post, title: old_title) }
+    let(:old_title) { 'Post wront title' }
+    let(:new_title) { 'Post new title' }
+
+    it 'returns 303' do
+      subject
+
+      expect(response).to have_http_status(:see_other)
+    end
+
+    it 'updates the Post successfully' do
+      subject
+
+      expect(post.reload.title).to eq(new_title)
+    end
+
+    it 'redirects to post show' do
+      subject
+
+      expect(response).to redirect_to(post_path(id: post.id))
+    end
+  end
+
+  describe 'DELETE /posts/:id' do
+    subject { delete "/posts/#{post.id}" }
+    let!(:post) { create(:post) }
+
+    it 'returns 303' do
+      subject
+
+      expect(response).to have_http_status(:see_other)
+    end
+
+    it 'deletes the Post successfully' do
+      expect{ subject }.to change { Post.count }.from(1).to(0)
+    end
+
+    it 'redirects to post show' do
+      subject
+
+      expect(response).to redirect_to(posts_path)
+    end
+  end
+end
